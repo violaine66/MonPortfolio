a29c10f6e2246b4fb05273c0b32ef51ae7cf1eff
violaine soulas <vhsoulas@gmail.com>
2025-07-05 18:40:39 +0200

    Photo profil + color BG + Nom Prenom ajoutés

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import nodejsIcon from "@iconify/icons-logos/nodejs-icon";
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+      var profilepic = process.env.PUBLIC_URL + "/images/" + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
