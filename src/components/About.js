7c17c9e5104da30ee9c4003b8b2525bb1f2e9624
violaine soulas <vhsoulas@gmail.com>
2025-07-05 20:44:56 +0200

    Modif icon

--- src/components/About.js
+++ src/components/About.js
@@ -42,7 +42,7 @@ class About extends Component {
                   />
                    <Icon
                     icon={javascriptIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: "300%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
                     icon={rubyIcon}
