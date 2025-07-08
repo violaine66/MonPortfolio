46001f91e19bf2be239a236339afef78083df979
violaine soulas <vhsoulas@gmail.com>
2025-07-08 12:32:43 +0200

    Updates

---
+++ precache-manifest.1a4d43aca1cdbbae692f3d2f270dc63c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75cf4296939903199893fe1652343d8",
+    "url": "/MonPortfolio/index.html"
+  },
+  {
+    "revision": "e4aff321488930d79f1a",
+    "url": "/MonPortfolio/static/css/2.e9dbce60.chunk.css"
+  },
+  {
+    "revision": "a322ec505af9061d0463",
+    "url": "/MonPortfolio/static/css/main.e15b32eb.chunk.css"
+  },
+  {
+    "revision": "e4aff321488930d79f1a",
+    "url": "/MonPortfolio/static/js/2.c07551b1.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/MonPortfolio/static/js/2.c07551b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a322ec505af9061d0463",
+    "url": "/MonPortfolio/static/js/main.c65523e4.chunk.js"
+  },
+  {
+    "revision": "f4491f3564036009ce35",
+    "url": "/MonPortfolio/static/js/runtime-main.8c4b426b.js"
+  }
+]);
